8c954a281e82ccc7922906205b645a97cf242ce9
Idan Stark <idanstark42@gmail.com>
2026-04-21 02:24:42 +0300

    end of day, I'm tired, going to sleep

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useState, useEffect } from 'react'
+
+import { NPCs } from './Backend'
+
 import './App.css'
 
+const EMPTY_BATTLE = { npcs: [], exchange: 1 }
+
+const MODES = { SEARCH: "search", BATTLE: "battle" }
+
+const ICONS = {
+  minor: "",
+  major: "",
+  master: "",
+  group: "",
+  NEXT: "",
+  RESET: ""
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [mode, setMode] = useState(MODES.SEARCH)
+  const [battle, setBattle] = useState({ ...EMPTY_BATTLE })
+  const [search, setSearch] = useState("")
+  const [results, setResults] = useState([])
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+  useEffect(() => {
+    (async () => {
+      setResults(await NPCs.filter(search))
+    })()
+  }, [search, setResults])
 
-      <div className="ticks"></div>
+  const generateNPC = template => ({
+    ...template,
+    ...(NPCs.tier_templates[template.tier]),
+    conditions: Object.fromEntries(template.conditions.map(condition => [condition, false]))
+  })
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+  const reset = () => {
+    setBattle({ ...EMPTY_BATTLE })
+  }
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+  const start = () => {
+    setMode(MODES.BATTLE)    
+  }
+
+  const select = (npc, approach) => {
+    npc.selectedApproach = (npc.selectedApproach === approach) ? undefined : approach
+    setBattle(prev => ({ ...prev }))
+  }
+
+  const ready = () => battle.npcs.every(npc => npc.selectedApproach)
+
+  const next = () => {
+    battle.npcs.forEach(npc => { npc.selectedApproach = undefined })
+    setBattle(prev => ({ ...prev, exchange: prev.exchange + 1 }))
+  }
+
+  return <div className={`main ${mode}`}>
+    <div className='search'>
+      <div className='search-line'>
+        <input name="search" value={search} onChange={e => setSearch(e.target.value)} />
+      </div>
+      <div className='search-results'>
+        {results.map(npc => <div className='search-result' onMouseUp={() => setBattle(prev => ({ ...prev, npcs: [...prev.npcs, generateNPC(npc)] }))}>
+          <div className='title'>{npc.title}</div>
+          <div className='tier'>{ICONS[npc.tier]} {npc.tier}</div>
+          <div className='description'>{npc.description}</div>
+          <div className='drive'>{npc.drive}</div>
+          {npc.principle ? <div className='principle'>{npc.principle}</div> : ''}
+          <div className='techniques'>{npc.techniques.join(',')}</div>
+          <div className='techniques'>{npc.conditions.join(',')}</div>
+        </div>)}
+      </div>
+    </div>
+    <div className={`battle ${ready() ? 'ready' : ''}`}>
+      <div className='npcs'>
+        {battle.npcs.map(npc => <div className='npc'>
+          <div className='title'>{npc.title}</div>
+          <div className='tier'>{ICONS[npc.tier]} {npc.tier}</div>
+          <div className='description'>{npc.description}</div>
+          <div className='drive'>{npc.drive}</div>
+          {npc.principle ? <div className='principle'>{npc.principle}</div> : ''}
+          <div className='techniques'>{npc.techniques.join(',')}</div>
+          <div className='approaches'>
+            {['Defend & Maneuver', 'Advance & Attack' , 'Evade & Observe'].map(approach => <div className={`approach ${approach} ${npc.selectedApproach === approach ? 'selected' : ''}`} onClick={() => select(npc, approach)}>
+              {approach}
+            </div>)}
+          </div>
+        </div>)}
+      </div>
+      <div className='actions'>
+        <div className='exchange-number'>Exchange #{battle.exchange}</div>
+        <div className='reset' onClick={next}>{ICONS.NEXT} next</div>
+        <div className='reset' onClick={reset}>{ICONS.RESET} reset</div>
+      </div>
+    </div>
+  </div>
 }
 
 export default App
---
+++ src/Backend.jsx
@@ -0,0 +1,236 @@
+const MIN_QUERY_LENGTH = 3
+
+export class NPCs {
+  static async filter(query) {
+    if (query.length < MIN_QUERY_LENGTH) {
+      return []
+    } else {
+      console.log(ALL_NPCs)
+      return ALL_NPCs.filter(npc => npc.title.toLowerCase().includes(query.toLowerCase()))
+    }
+  }
+
+  static tier_templates = {
+    minor: { maxFatigue: 3, balance: 0 }
+  }
+}
+
+const ALL_NPCs = [
+  // ===== MINOR =====
+  {
+    title: "Town Guard",
+    tier: "minor",
+    description: "A local guard",
+    drive: "To keep order in the town",
+    principle: null,
+    conditions: ["Troubled"],
+    fatigue: 0,
+    techniques: []
+  },
+  {
+    title: "Village Hunter",
+    tier: "minor",
+    description: "A skilled trapper and hunter",
+    drive: "To see their village thrive",
+    principle: null,
+    conditions: ["Angry"],
+    fatigue: 0,
+    techniques: []
+  },
+  {
+    title: "Trader",
+    tier: "minor",
+    description: "A traveling merchant",
+    drive: "To become prosperous",
+    principle: null,
+    conditions: ["Afraid"],
+    fatigue: 0,
+    techniques: []
+  },
+  {
+    title: "Shopkeeper",
+    tier: "minor",
+    description: "An owner of a successful city shop",
+    drive: "To maintain their social standing",
+    principle: null,
+    conditions: ["Insecure"],
+    fatigue: 0,
+    techniques: []
+  },
+  {
+    title: "Tough",
+    tier: "minor",
+    description: "Local muscle",
+    drive: "To follow orders",
+    principle: null,
+    conditions: ["Afraid"],
+    fatigue: 0,
+    techniques: []
+  },
+  {
+    title: "Soldier",
+    tier: "minor",
+    description: "A trained grunt of a larger unit",
+    drive: "To follow orders",
+    principle: null,
+    conditions: ["Guilty"],
+    fatigue: 0,
+    techniques: []
+  },
+
+  // ===== MAJOR =====
+  {
+    title: "Outlaw Captain",
+    tier: "major",
+    description: "A leader of pirates, bandits, or criminals",
+    drive: "To take what others can’t hold",
+    principle: "Survival",
+    conditions: ["Angry", "Afraid", "Foolish"],
+    fatigue: 0,
+    techniques: ["Sense Environment", "Duck and Twist"]
+  },
+  {
+    title: "Champion Pit-Fighter",
+    tier: "major",
+    description: "A champion pit-fighter",
+    drive: "To control the pit-fighting scene",
+    principle: "Discipline",
+    conditions: ["Angry", "Desperate", "Guilty"],
+    fatigue: 0,
+    techniques: ["Charge", "Forceful Blow"]
+  },
+  {
+    title: "Military Commander",
+    tier: "major",
+    description: "A trained and capable military commander",
+    drive: "To protect their subordinates",
+    principle: "Duty",
+    conditions: ["Angry", "Jaded", "Troubled"],
+    fatigue: 0,
+    techniques: ["Rapid Assessment"]
+  },
+  {
+    title: "Noble",
+    tier: "major",
+    description: "A member of the ruling class",
+    drive: "To seek excitement and novelty",
+    principle: "Freedom",
+    conditions: ["Despondent", "Hopeless", "Insecure"],
+    fatigue: 0,
+    techniques: []
+  },
+  {
+    title: "Political Leader",
+    tier: "major",
+    description: "A local mayor, magistrate, or governor",
+    drive: "To minimize problems",
+    principle: "Community",
+    conditions: ["Afraid", "Frantic", "Insecure"],
+    fatigue: 0,
+    techniques: []
+  },
+
+  // ===== MASTER =====
+  {
+    title: "Accomplished General",
+    tier: "master",
+    description: "A weaponmaster in command of extensive forces",
+    drive: "To win favor with their commander",
+    principle: "Ambition",
+    conditions: ["Afraid", "Angry", "Disgusted", "Guilty", "Morose"],
+    fatigue: 0,
+    techniques: ["Feint", "Turn the Tables", "Pinpoint Thrust"]
+  },
+  {
+    title: "Obsessive Inventor",
+    tier: "master",
+    description: "A creator of dangerous and innovative devices",
+    drive: "To create perfection",
+    principle: "Progress",
+    conditions: ["Afraid", "Angry", "Insecure", "Manic", "Offended"],
+    fatigue: 0,
+    techniques: ["Jolt", "Collect Materials", "Wind Up"]
+  },
+  {
+    title: "Rebel Leader",
+    tier: "master",
+    description: "A rebel war leader in the Earth Kingdom",
+    drive: "To overthrow the existing government",
+    principle: "Justice",
+    conditions: ["Afraid", "Angry", "Guilty", "Hopeless", "Humiliated"],
+    fatigue: 0,
+    techniques: ["Earth Armor", "Stone Shield"]
+  },
+  {
+    title: "Triad Leader",
+    tier: "master",
+    description: "An infamous firebending Republic City criminal",
+    drive: "To maintain their empire",
+    principle: "Role",
+    conditions: ["Afraid", "Fixated", "Frustrated", "Insecure", "Vengeful"],
+    fatigue: 0,
+    techniques: ["Lightning Blast", "Flame Knives"]
+  },
+  {
+    title: "Water Tribe Chief",
+    tier: "master",
+    description: "An experienced and trusted Water Tribe leader",
+    drive: "To protect their people",
+    principle: "Tradition",
+    conditions: ["Angry", "Guilty", "Insecure", "Stubborn", "Troubled"],
+    fatigue: 0,
+    techniques: ["Crushing Grip of Seas", "Stream the Water"]
+  },
+
+  // ===== GROUPS =====
+  {
+    title: "Small Mob",
+    tier: "group",
+    description: "A small mob of minor thugs",
+    drive: "To punish those who wronged them",
+    principle: "Retribution",
+    conditions: ["Angry", "Insecure", "Troubled"],
+    fatigue: 0,
+    techniques: ["Overwhelm"]
+  },
+  {
+    title: "Military Squad",
+    tier: "group",
+    description: "A small squad of trained soldiers",
+    drive: "To accomplish their given objective",
+    principle: "Duty",
+    conditions: ["Afraid", "Guilty", "Insecure"],
+    fatigue: 0,
+    techniques: ["Focused Fire", "Protect Objective"]
+  },
+  {
+    title: "Palace Guards",
+    tier: "group",
+    description: "A medium group of trained guards",
+    drive: "To protect the palace",
+    principle: "Loyalty",
+    conditions: ["Afraid", "Angry", "Desperate", "Guilty", "Humiliated"],
+    fatigue: 0,
+    techniques: ["Coordination", "Shield Wall", "Swarm"]
+  },
+  {
+    title: "Republic City Police Squad",
+    tier: "group",
+    description: "A group of metalbenders",
+    drive: "To enforce law and order",
+    principle: "Results",
+    conditions: ["Distracted", "Guilty", "Overbearing", "Troubled", "Zealous"],
+    fatigue: 0,
+    techniques: ["Metal Bindings", "Spread Out", "Test Defenses"]
+  },
+  {
+    title: "Elite Rebels",
+    tier: "group",
+    description: "A group of elite revolutionaries",
+    drive: "To overturn authority",
+    principle: "Freedom",
+    conditions: ["Afraid", "Guilty", "Hopeless", "Insecure", "Overconfident"],
+    fatigue: 0,
+    techniques: ["Scatter and Regroup", "Swarm", "Surround"]
+  }
+];
